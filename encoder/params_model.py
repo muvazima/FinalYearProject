874278b33ccb59535c25bd918c989df4715f1e09
Manam Mansoor <Manam@Manams-MacBook-Air.local>
2020-09-20 20:08:45 +0530

    add

--- encoder/params_model.py
+++ encoder/params_model.py
@@ -1,11 +1,11 @@
 
 ## Model parameters
-model_hidden_size = 256
-model_embedding_size = 256
+#model_hidden_size = 256
+#model_embedding_size = 256
 model_num_layers = 3
 
-#model_hidden_size = 256
-#model_embedding_size = 768
+model_hidden_size = 256
+model_embedding_size = 768
 ## Training parameters
 learning_rate_init = 1e-4
 speakers_per_batch = 64
